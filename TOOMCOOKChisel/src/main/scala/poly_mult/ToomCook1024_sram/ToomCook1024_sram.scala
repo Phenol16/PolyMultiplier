3f4a21ae623cbc62abf9ab8c5995a5741119c601
Phenol16 <131586433+Phenol16@users.noreply.github.com>
2026-05-15 22:01:14 +0800

    refactor: reuse Core16 in ToomCook1024 SRAM pipeline

--- TOOMCOOKChisel/src/main/scala/poly_mult/ToomCook1024_sram/ToomCook1024_sram.scala
+++ TOOMCOOKChisel/src/main/scala/poly_mult/ToomCook1024_sram/ToomCook1024_sram.scala
@@ -2,6 +2,7 @@ package poly_mult_sram
 
 import chisel3._
 import chisel3.util._
+import poly_mult.Core16
 
 object Util {
   def mask(value: UInt, targetWidth: Int): UInt = {
@@ -240,101 +241,6 @@ class Interp8ColsStepTC4(pidx: Int, inW: Int, outW: Int) extends Module {
   io.nr2 := carry2(8)
 }
 
-class Product4TC4 extends Module {
-  private val A_EVAL_W = TC4EvalWidth.A_EVAL_W
-  private val B_EVAL_W = TC4EvalWidth.B_EVAL_W
-  private val PROD_MUL_MOD_W = A_EVAL_W
-  private val PROD_OUT_W = 36
-
-  val io = IO(new Bundle {
-    val a4 = Input(Vec(4, UInt(A_EVAL_W.W)))
-    val b4 = Input(Vec(4, UInt(B_EVAL_W.W)))
-    val out = Output(Vec(7, UInt(PROD_OUT_W.W)))
-  })
-
-  val evalA = Module(new EvalLayerTC4(A_EVAL_W, A_EVAL_W))
-  val evalB = Module(new EvalLayerTC4(B_EVAL_W, B_EVAL_W))
-  evalA.io.r := io.a4
-  evalB.io.r := io.b4
-
-  val wMul = Wire(Vec(7, UInt(PROD_MUL_MOD_W.W)))
-  for (i <- 0 until 7) {
-    val bw = evalB.io.out(i)(B_EVAL_W - 1, 0)
-    val bwSext = Cat(Fill(A_EVAL_W - B_EVAL_W, bw(B_EVAL_W - 1)), bw).asSInt
-    wMul(i) := mask((evalA.io.out(i)(A_EVAL_W - 1, 0).asSInt * bwSext).asUInt, PROD_MUL_MOD_W)
-  }
-
-  val r5a = mask(wMul(5) - wMul(4), PROD_MUL_MOD_W)
-  val r3a = mask(mask(wMul(3) - wMul(2), PROD_MUL_MOD_W) >> 1, PROD_MUL_MOD_W)
-  val r4a = mask(wMul(4) - wMul(0), PROD_MUL_MOD_W)
-  val r4b = mask((r4a << 1) + r5a - (wMul(6) << 7), PROD_MUL_MOD_W)
-  val r2a = mask(wMul(2) + r3a, PROD_MUL_MOD_W)
-  val r1a = mask(wMul(1) + wMul(4) - (r2a << 6) - r2a, PROD_MUL_MOD_W)
-  val r2b = mask(r2a - wMul(6) - wMul(0), PROD_MUL_MOD_W)
-  val r1b = mask(r1a + r2b + (r2b << 2) + (r2b << 3) + (r2b << 5), PROD_MUL_MOD_W)
-  val r4c = mask(mask(mask(r4b - (r2b << 3), PROD_MUL_MOD_W) >> 3, PROD_MUL_MOD_W) * "hAAAAAAAAB".U(42.W), PROD_OUT_W)
-  val r5b = mask(mask((r5a + r1b) >> 1, PROD_MUL_MOD_W) * "hEEEEEEEEF".U(42.W), 37)
-  val r1c = mask(mask(mask(r1b + (r3a << 4), PROD_MUL_MOD_W) >> 1, PROD_MUL_MOD_W) * "hE38E38E39".U(42.W), 37)
-  val r2c = mask(r2b - r4c, PROD_OUT_W)
-  val r3b = mask(0.U - r3a - r1c, PROD_OUT_W)
-  val r5c = mask((r1c - r5b) >> 1, PROD_OUT_W)
-  val r1d = mask(r1c - r5c, PROD_OUT_W)
-
-  io.out(0) := mask(wMul(6) - r2c, PROD_OUT_W)
-  io.out(1) := mask(r5c - r1d, PROD_OUT_W)
-  io.out(2) := mask(r4c - wMul(0), PROD_OUT_W)
-  io.out(3) := r3b
-  io.out(4) := 0.U
-  io.out(5) := 0.U
-  io.out(6) := 0.U
-}
-
-class Core16TC4 extends Module {
-  private val A_EVAL_W = TC4EvalWidth.A_EVAL_W
-  private val B_EVAL_W = TC4EvalWidth.B_EVAL_W
-  private val CORE_OUT_W = 36
-
-  val io = IO(new Bundle {
-    val valid_in = Input(Bool())
-    val avec = Input(Vec(16, UInt(A_EVAL_W.W)))
-    val bvec = Input(Vec(16, UInt(B_EVAL_W.W)))
-    val valid_out = Output(Bool())
-    val cOut = Output(Vec(16, UInt(CORE_OUT_W.W)))
-  })
-
-  val ae = Wire(Vec(28, UInt(A_EVAL_W.W)))
-  val be = Wire(Vec(28, UInt(B_EVAL_W.W)))
-  for (seg <- 0 until 4) {
-    val evalA = Module(new EvalLayerTC4(A_EVAL_W, A_EVAL_W))
-    val evalB = Module(new EvalLayerTC4(B_EVAL_W, B_EVAL_W))
-    for (k <- 0 until 4) {
-      evalA.io.r(k) := io.avec(seg * 4 + k)
-      evalB.io.r(k) := io.bvec(seg * 4 + k)
-    }
-    for (pt <- 0 until 7) {
-      ae(pt * 4 + seg) := evalA.io.out(pt)
-      be(pt * 4 + seg) := evalB.io.out(pt)
-    }
-  }
-
-  val wProd = Wire(Vec(28, UInt(CORE_OUT_W.W)))
-  for (pt <- 0 until 7) {
-    val prod = Module(new Product4TC4)
-    for (k <- 0 until 4) {
-      prod.io.a4(k) := ae(pt * 4 + k)
-      prod.io.b4(k) := be(pt * 4 + k)
-      wProd(pt * 4 + k) := prod.io.out(k)
-    }
-  }
-
-  val regW = RegEnable(wProd, io.valid_in)
-  val regValid = RegNext(io.valid_in, false.B)
-  val interp = Module(new Interp4ColsTC4(pidx = 0, inW = 36, outW = 36))
-  interp.io.in := regW
-
-  io.valid_out := regValid
-  io.cOut := interp.io.out
-}
 
 class SpRam(width: Int, depth: Int) extends BlackBox(Map("WIDTH" -> width, "DEPTH" -> depth)) with HasBlackBoxResource {
   override def desiredName: String = "sp_ram"
@@ -452,7 +358,7 @@ class ToomCook43Clean extends Module {
     evalBVec(lane) := evalB(lane).io.out
   }
 
-  val core = Module(new Core16TC4)
+  val core = Module(new Core16)
   core.io.valid_in := false.B
   val coreAWordReg = Reg(UInt((16 * A_EVAL_W).W))
   val coreBWordReg = Reg(UInt((16 * B_EVAL_W).W))
